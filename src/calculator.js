33aa7e2eb9aa6d118d90e03b4cd9707f0f206a14
AtypicalLogic <archer.mansueto@gmail.com>
2026-03-22 18:03:13 +0800

    add calculator module and test

---
+++ src/calculator.js
@@ -0,0 +1,70 @@
+const calculator = (() => {
+  const checkTypeError = (a, b) =>
+    (a.constructor.name !== "Number") | (b.constructor.name !== "Number");
+  const logTypeError = (a, b) => {
+    const errA = a.constructor.name;
+    const errB = b.constructor.name;
+    console.error("Arg text has incorrect argument", errA, errB);
+  };
+  const checkUndefinedError = (a, b) => !a | !b;
+  const logUndefinedError = (a, b) => {
+    const errorMsg = "All arguments must be defined";
+    console.error(
+      "errorMsg" + !a
+        ? "a is undefined"
+        : `a is ${a}` + !b
+          ? "b is undefined"
+          : `b is ${b}`,
+    );
+  };
+  const add = (a, b) => {
+    if (checkUndefinedError(a, b)) {
+      logUndefinedError(a, b);
+      return;
+    }
+    if (checkTypeError(a, b)) {
+      logTypeError(a, b);
+      return;
+    }
+    return a + b;
+  };
+  const subtract = (a, b) => {
+    if (checkUndefinedError(a, b)) {
+      logUndefinedError(a, b);
+      return;
+    }
+    if (checkTypeError(a, b)) {
+      logTypeError(a, b);
+      return;
+    }
+    return a - b;
+  };
+  const multiply = (a, b) => {
+    if (checkUndefinedError(a, b)) {
+      logUndefinedError(a, b);
+      return;
+    }
+    if (checkTypeError(a, b)) {
+      logTypeError(a, b);
+      return;
+    }
+    return a * b;
+  };
+  const divide = (a, b) => {
+    if (checkUndefinedError(a, b)) {
+      logUndefinedError(a, b);
+      return;
+    }
+    if (checkTypeError(a, b)) {
+      logTypeError(a, b);
+      return;
+    }
+    return a / b;
+  };
+
+  return { add, subtract, multiply, divide };
+})();
+
+// console.log(calculator.add());
+
+export default calculator;
---
+++ tests/calculator.test.js
@@ -0,0 +1,61 @@
+import calculator from "/src/calculator";
+
+describe("calculator", () => {
+  test("happy: module exists", () => {
+    expect(calculator).toBeDefined();
+  });
+
+  test("happy: module is an Object", () => {
+    expect(calculator.constructor.name).toBe("Object");
+  });
+
+  test("happy: module has specific methods", () => {
+    const methods = ["add", "subtract", "multiply", "divide"];
+    methods.forEach((method) => expect(calculator[method]).toBeDefined());
+  });
+
+  test("happy: operations with correct expected values", () => {
+    const ops = [
+      { op: "add", a: 64, b: 4, res: 68 },
+      { op: "subtract", a: 64, b: 4, res: 60 },
+      { op: "multiply", a: 64, b: 4, res: 256 },
+      { op: "divide", a: 64, b: 4, res: 16 },
+    ];
+    ops.forEach((val) => {
+      // console.log(`${val.op} ${val.a}, ${val.b}, ${val.res}`)
+      expect(calculator[val.op](val.a, val.b)).toBe(val.res);
+    });
+  });
+
+  test("sad: error handling for undefined", () => {
+    const consoleSpy = jest
+      .spyOn(console, "error")
+      .mockImplementation(() => {});
+    const errorMsg = "Arg text has incorrect argument";
+    expect(calculator["add"]()).toBeUndefined();
+  });
+
+  test("sad: error handling for non-numeric types", () => {
+    const consoleSpy = jest
+      .spyOn(console, "error")
+      .mockImplementation(() => {});
+    const errorMsg = "Arg text has incorrect argument";
+    const dtypes = [[], true, {}, "a"];
+    const methods = ["add", "subtract", "multiply", "divide"];
+    dtypes.forEach((ty, idx) => {
+      methods.forEach((met) => {
+        expect(calculator[met](ty, ty)).toBeUndefined();
+        expect(consoleSpy).toHaveBeenCalled();
+        expect(consoleSpy).toHaveBeenCalledWith(
+          errorMsg,
+          dtypes[idx].constructor.name,
+          dtypes[idx].constructor.name,
+        );
+      });
+    });
+  });
+
+  test("sad: error handling for empty strings", () => {
+    expect(calculator.add("", "")).toBeUndefined();
+  });
+});
